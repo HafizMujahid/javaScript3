05974b9ec5af0d00da9161dd5e3596ba79a05a5c
HafizMujahid <161130471+HafizMujahid@users.noreply.github.com>
2024-03-23 02:02:54 -0700

    Add files via upload
    contains 17 chapter of the book

---
+++ app.js
@@ -0,0 +1,169 @@
+/ //1 
+
+// let array = [[[]]];
+
+// //2
+
+// let array1 = [0,1,2,3 [1,0,1,2 [2,1,0,1]]];
+// console.log(array1);
+
+
+//3
+
+// let num = [1,2,3,4,5,6,7,8,9,10]
+
+// for(i = 0 ; i < num.length; i++) {
+//     console.log(num[i]);
+// }
+
+//4
+
+// let number = +prompt("Enter a number to print its table:");
+
+// let length = +prompt("Enter length of table:");
+
+// for(i = 1 ; i <= length ; i++ ){
+//     console.log(number + "x" + i + "=" + number * i);
+// }
+
+//5
+
+// let fruits = [ Apple , Banana , Mango , Orange , Strawberry];
+
+// for(i = 0 ; i < fruits.length ; i++){
+//     console.log(fruits[i]);
+
+// }
+
+//6
+
+// let num = [];
+
+// for (i = 1 ; i <= 15 ; i++) {
+
+//     num.push[i];
+
+//     console.log(num);
+// }
+
+//7
+
+// let items = ['Cake' , 'Apple-pie' , 'Cookies' , 'Chips' , 'Patties'];
+
+// let userInput = prompt("Enter a item you want to search and please type first character in Uppercase:")
+
+// for(i = 0 ; i < items.length ; i++){
+//     if(userInput === items[i]){
+//         console.log("found")
+//     }
+//     else {
+//         console.log("not found")
+//     }
+// }
+
+//10
+
+// let num = 5;
+
+// for (i = 1 ; i <= 20 ; i++) {
+//     console.log(num * i);
+// }
+
+//1
+
+// let firstName = prompt("Enter your first name:"); 
+// let secondName = prompt("Enter your second name:");
+
+// let fullName = firstName + " " + secondName;
+
+// alert("HELLO! " + fullName);
+
+//2
+
+// let mobile = prompt("Enter your favourite mobile phone model:");
+
+// let Length = mobile.length;
+
+// console.log(Length);
+
+
+//3
+
+// let word = "Pakistani";
+
+// let index = word.indexOf('n');
+
+// console.log("Index of letter 'n' is: " + index);
+
+//4
+
+// let word = "Hello World";
+
+// let index = word.lastIndexOf('l');
+
+// console.log('Lastindex of l is: ' + index);
+
+//5
+
+// let word = "Pakistani";
+
+// console.log("Character at index 3 is: " + word[3]);
+
+//6
+
+// let firstName = prompt("Enter your first name:"); 
+// let secondName = prompt("Enter your second name:");
+
+// console.log(firstName.concat() , secondName);
+
+//7
+
+// let word = 'Hyderabad';
+
+// let replacement = word.replace('Hyder' , 'Islam');
+
+// console.log(replacement);
+
+//8
+
+// let message = 'Ali and Sami are best friends.They play Cricket and football together.';
+
+// let replacement = message.replaceAll('and' , '&');
+
+// console.log(replacement);
+
+//9
+
+// var str = "472";
+// console.log(str);
+// console.log(typeof(str));
+
+// let number = Number(str);
+// console.log(number);
+// console.log(typeof(number));
+
+//10
+
+// let userInput = prompt( "Enter a word:");
+
+// console.log("User Input: " + userInput);
+
+// console.log("Uppercase"+ '\n' + userInput.toUpperCase(userInput));
+
+//11
+
+// let userInput = prompt("Enter a word:");
+
+// console.log("User Input: " + userInput);
+
+
+//12
+
+// let num = 235.34567;
+
+// let str = num.toString();
+
+// console.log(typeof(str));
+
+// replacement = str.replace('.' , '');
+// console.log(replacement);
